9efdd77e636202a511a45ab9223221e09caa6401
Dongwan Kim <dongwan_kim@tmax.co.kr>
2022-03-21 09:02:08 +0900

    temporary concept

--- src/20/concepts.cpp
+++ src/20/concepts.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 /* before using concept*/
 template<typename T>
 requires (!std::is_pointer_v<T>)
@@ -53,15 +53,44 @@ concept IsPointerObject = requires(T p) { *p; };
 
 
 
+/* nawrrowing occurs */
+template<typename F, typename T>
+requires std::convertible_to<F, T>
+void add(F, T)
+{
+     std::cout << "may be narrowing\n";
+}
+
+/* without narrowing constrain*/
+/* Because it uses convertible_to and more, it subsumes above template ... so, it is more constrained  */
+template<typename From, typename To>
+concept ConvertsWithoutNarrowing =
+    std::convertible_to<From, To> &&
+        requires (From&& x) {
+                { std::type_identity_t<To[]>{std::forward<From>(x)} }
+                         -> std::same_as<To[1]>;
+                             };
+
+template<typename Coll, typename T>
+requires ConvertsWithoutNarrowing<T, typename Coll::value_type>
+void add(Coll& coll, const T& val)
+{
+  std::cout<<"without narrowing"<<std::endl;
+  coll.push_back(val);
+}
+
 
 int main(){
-
+#if 0
   int x = 42;
   int y = 77;
   std::cout << maxValue(x, y) << '\n';      // OK: maximum value of ints
   std::cout << maxValue_concept(x, y) << '\n';      // OK: maximum value of ints
 //  std::cout << maxValue(&x, &y) << '\n';    // ERROR: constraint not met
   std::cout << maxValue_concept(&x, &y) << '\n';    // ERROR: constraint not met
-
+#endif
+  std::vector<int> xx;
+  add(xx, 1);
+//  add(xx,1ul);
   return 0;
 }
